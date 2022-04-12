2feb897560a193c96ae3919856924ccd9f1085f7
Samuelsntr <dodisamuel20@gmail.com>
2022-04-12 17:05:32 +0800

    membuat box

---
+++ script.js
@@ -0,0 +1,12 @@
+const box = document.getElementById('box');
+document.onmousemove = (event) => {
+    box.style.left = event.clientX - (box.clientWidth/2) + 'px'; 
+    box.style.top  = event.clientY - (box.clientHeight/2)+ 'px';
+}
+function random(number) {
+    return Math.floor(Math.random()* number)
+}
+document.onclick  = function () { 
+cakacak = "rgb("+ random(255)+","+ random(255)+","+ random(255)+")";
+box.style.background = cakacak
+}
