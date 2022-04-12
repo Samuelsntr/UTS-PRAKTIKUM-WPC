7a2d2543c53c3e3bc4c63d75a941055a7f1ee75d
Samuelsntr <dodisamuel20@gmail.com>
2022-04-12 17:22:44 +0800

    change background color

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ document.onmousemove = (event) => {
     box.style.left = event.clientX - (box.clientWidth/2) + 'px'; 
     box.style.top  = event.clientY - (box.clientHeight/2)+ 'px';
 }
+
+function changeColor(){
+    box.style.backgroundColor = 'salmon';
+}
+
+const div = document.querySelector('#box');
+div.onclick = changeColor
